8daa8ad133d1d937e9540fa2ba931cb4d132a841
Andreas Koj <kojandreas@gmail.com>
2023-11-06 21:28:53 +0100

    rework the text inputs to part of the table

--- src/matches/Matches.js
+++ src/matches/Matches.js
@@ -5,10 +5,37 @@ export default function Matches({ data, setData }) {
 
     const [showForm, setShowForm] = React.useState(false);
     const [showAddButton, setShowAddButton] = React.useState(true);
+    const [showCancelButton, setShowCancelButton] = React.useState(false);
+    const [showConfirmButton, setConfirmButton] = React.useState(false);
 
-    const handleClick = () => {
+    const handleAddClick = () => {
         setShowAddButton(false)
+        setShowCancelButton(true)
+        setConfirmButton(true)
         setShowForm(true)
+    }
+
+    const handleCancelClick = () => {
+        setShowAddButton(true)
+        setShowCancelButton(false)
+        setConfirmButton(false)
+        setShowForm(false)
+    }
+
+    const handleConfirmClick = () => {
+        setShowAddButton(true)
+        setShowCancelButton(false)
+        setConfirmButton(false)
+        setShowForm(false)
+    }
+
+    const getTodaysDate = () => {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
     }
 
     return (
@@ -34,76 +61,81 @@ export default function Matches({ data, setData }) {
                             </tr>
                         ))}
                         {showForm && <tr key="100">
-                            <td>Test this </td>
-                            <td>Test</td>
-                            <td>Test</td>
-                            <td>Test</td>
-                            </tr>}
+                            <td><input type='Text' name="Player1" /></td>
+                            <td><input type='Text' name="Player2" /></td>
+                            <td>
+                                <input type='Text' name="ScorePlayer1" />:
+                                <input type='Text' name="ScorePlayer2" />
+                            </td>
+                            <td><input type='Text' name="Date" value={getTodaysDate()} /></td>
+                        </tr>}
                     </tbody>
                 </table>
-                {showAddButton && <button onClick={handleClick}>Add</button>}
-                {showForm && <Form setShowForm={setShowForm} setShowAddButton={setShowAddButton} setData={setData} data={data} />}
+                {showAddButton && <button onClick={handleAddClick}>Add</button>}
+                {showConfirmButton && <button onClick={handleConfirmClick}>Confirm</button>}
+                {showCancelButton && <button onClick={handleCancelClick}>Cancel</button>}
+                {/* {showForm && <Form setShowForm={setShowForm} setShowAddButton={setShowAddButton} setData={setData} data={data} />} */}
             </div>
         </div>
     )
 }
 
-const Form = ({ setShowForm, setShowAddButton, setData, data }) => {
+// const Form = ({ setShowForm, setShowAddButton, setData, data }) => {
 
-    const handleCancelClick = () => {
-        setShowForm(false);
-        setShowAddButton(true)
-    }
+//     const handleCancelClick = () => {
+//         setShowForm(false);
+//         setShowAddButton(true)
+//     }
 
-    const handleSubmitForm = (e) => {
-        e.preventDefault();
+//     const handleSubmitForm = (e) => {
+//         e.preventDefault();
 
-        const form = new FormData(e.target);
-        const formArray = [...form.entries()]
+//         const form = new FormData(e.target);
+//         const formArray = [...form.entries()]
 
-        const match = {
-            "Player1":
-            {
-                "Name": formArray[0][1],
-                "Score": formArray[2][1].charAt(0),
-            },
-            "Player2":
-            {
-                "Name": formArray[1][1],
-                "Score": formArray[2][1].charAt(2),
-            },
-            "Date": formArray[3][1]
-        };
+//         const match = {
+//             "Player1":
+//             {
+//                 "Name": formArray[0][1],
+//                 "Score": formArray[2][1].charAt(0),
+//             },
+//             "Player2":
+//             {
+//                 "Name": formArray[1][1],
+//                 "Score": formArray[2][1].charAt(2),
+//             },
+//             "Date": formArray[3][1]
+//         };
 
-        setData([...data, match])
+//         setData([...data, match])
 
-        setShowForm(false)
-        setShowAddButton(true)
-    }
+//         setShowForm(false)
+//         setShowAddButton(true)
+//     }
 
-    return (
-        <div className='matchInputWrapper'>
-            <form onSubmit={handleSubmitForm}>
-                <div className='textInputContainer'>
-                    <input type="text" name="Player1" />
-                    <input type="text" name="Player2" />
-                    <input type="text" name="Score" />
-                    <input type="text" value={GetTodaysDate()} name="Date" />
-                </div>
-                <div className='buttonInputContainer'>
-                    <input type="submit" value="Submit" />
-                    <input type="cancel" onClick={handleCancelClick} value="Cancel" />
-                </div>
-            </form>
-        </div>
-    );
+//     return (
+//         <div className='matchInputWrapper'>
+//             <form onSubmit={handleSubmitForm}>
+//                 <div className='textInputContainer'>
+//                     <input type="text" name="Player1" />
+//                     <input type="text" name="Player2" />
+//                     <input type="text" name="Score" />
+//                     <input type="text" value={GetTodaysDate()} name="Date" />
+//                 </div>
+//                 <div className='buttonInputContainer'>
+//                     <input type="submit" value="Submit" />
+//                     <input type="cancel" onClick={handleCancelClick} value="Cancel" />
+//                 </div>
+//             </form>
+//         </div>
+//     );
 
-    function GetTodaysDate() {
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const day = String(currentDate.getDate()).padStart(2, '0');
+//     function GetTodaysDate() {
+//         const currentDate = new Date();
+//         const year = currentDate.getFullYear();
+//         const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+//         const day = String(currentDate.getDate()).padStart(2, '0');
 
-        return `${year}-${month}-${day}`;
-    }
-};
+//         return `${year}-${month}-${day}`;
+//     }
+// };
